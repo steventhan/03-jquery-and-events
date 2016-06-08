44382ecb6ab86df6eac2afd720808ee2b75cc80a
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-06-08 13:52:22 -0700

    filter for main nav is working

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -58,7 +58,11 @@ articleView.handleMainNav = function() {
       1. Hide all the .tab-content sections.
       2. Fade in the single .tab-content section that is associated with the
       clicked .tab element's data-content attribute. */
-  $('.main-nav'). on(/* CODE GOES HERE*/);
+  $('.main-nav').on('click','.tab', function(){
+    $('.tab-content').hide();
+    val = $(this).attr('data-content');
+    $('#' + val).fadeIn();
+  });
 
   $('.main-nav .tab:first').click();
 };
@@ -80,3 +84,4 @@ articleView.setTeasers = function() {
 articleView.populateFilters();
 articleView.handleAuthorFilter();
 articleView.handleCategoryFilter();
+articleView.handleMainNav();
